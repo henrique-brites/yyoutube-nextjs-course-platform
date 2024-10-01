ad8e2edeead510dc92f2fffad9c566585d135591
Henrique Brites <henriquebrites@live.com>
2024-10-01 01:22:56 -0300

    feat: add navigation drawer

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,4 +1,8 @@
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "CodarSe - Todos os cursos"
+};
 
 export default function PageCourses() {
   return (
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,27 @@
 "use client";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 import { MdMenu, MdOutlineOpenInNew } from "react-icons/md";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 
 export const Header = () => {
+  const [title, setTitle] = useState("CodarSe");
+  const [drawer, setDrawer] = useState(false);
   const currentPath = usePathname();
+
+  useEffect(() => {
+    setTitle(document.title);
+    setDrawer(false);
+  }, [currentPath]);
 
   return (
     <nav className="flex items-center gap-6 justify-start md:justify-center bg-primary py-2 sm:py-4 px-6">
-      <button className="sm:hidden">
+      <button className="sm:hidden" onClick={() => setDrawer(true)}>
         <MdMenu size={24} />
       </button>
 
-      <ul className="flex gap-4 items-center">
+      <ul className="flex gap-4 items-center" tabIndex={drawer ? -1 : undefined}>
         <li className="my-2">
           <Link href="/" className="border-2 rounded-md py-2 px-1 font-bold">
             CODARSE
@@ -37,7 +45,35 @@ export const Header = () => {
         </li>
       </ul>
 
-      <h1 className="sm:hidden">Codarse - Página inicial</h1>
+      <div 
+        data-open={drawer}
+        tabIndex={drawer ? undefined : -1}
+        onClick={() => setDrawer(false)}
+        className="bg-gradient-to-r from-background fixed top-0 left-0 bottom-0 right-0 transition-transform data-[open=false]:-translate-x-full"
+      >
+        <ul className="flex gap-2 flex-col p-4 w-60 h-full bg-background" onClick={(e) => e.stopPropagation()}>        
+          <li data-active={currentPath === "/"} className="py-1 data-[active=true]:border-b">
+            <Link href="/">
+              Página inicial
+            </Link>
+          </li>        
+          <li data-active={currentPath === "/courses"} className="py-1 data-[active=true]:border-b">
+            <Link href="/courses">
+              Cursos
+            </Link>
+          </li>
+          <li className="">
+            <Link href="https://blog.codarse.com" target="_blank" className=" py-1 flex gap-1 items-center">
+              Blog
+              <MdOutlineOpenInNew />
+            </Link>
+          </li>
+        </ul>
+      </div>      
+
+      <h1 className="sm:hidden">
+        {title}
+      </h1>
     </nav>
   );
 };
