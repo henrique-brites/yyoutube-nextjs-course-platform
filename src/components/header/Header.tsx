c3a1e94d6d27cbfdbf947e57037dbed3ba17a9a6
Henrique Brites <henriquebrites@live.com>
2024-09-30 21:14:42 -0300

    feat: add responsive header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,37 +1,43 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { MdOutlineOpenInNew } from "react-icons/md";
+import { MdMenu, MdOutlineOpenInNew } from "react-icons/md";
 
 
 export const Header = () => {
   const currentPath = usePathname();
 
   return (
-    <nav className="flex items-center justify-center bg-primary py-4">
+    <nav className="flex items-center gap-6 justify-start md:justify-center bg-primary py-2 sm:py-4 px-6">
+      <button className="sm:hidden">
+        <MdMenu size={24} />
+      </button>
+
       <ul className="flex gap-4 items-center">
-      <li className="my-2">
+        <li className="my-2">
           <Link href="/" className="border-2 rounded-md py-2 px-1 font-bold">
             CODARSE
           </Link>
         </li>
-        <li>
+        <li className="hidden sm:block">
           <Link href="/" data-active={currentPath === "/"} className="data-[active=true]:underline">
             Página inicial
           </Link>
         </li>        
-        <li>
+        <li className="hidden sm:block">
           <Link href="/courses" data-active={currentPath === "/courses"} className="data-[active=true]:underline">
             Cursos
           </Link>
         </li>
-        <li>
+        <li className="hidden sm:block">
           <Link href="https://blog.codarse.com" target="_blank" className="flex gap-1 items-center">
             Blog
             <MdOutlineOpenInNew />
           </Link>
         </li>
       </ul>
+
+      <h1 className="sm:hidden">Codarse - Página inicial</h1>
     </nav>
   );
 };
